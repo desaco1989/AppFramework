9acf568b3bc3b306e82ab7ffd0ed2d76610927f6
unknown <dengwen@netwin.com>
2018-11-05 11:08:15 +0800

    update

---
+++ app/src/main/java/cn/netwins/allmedianews/base/BaseFragmentActivity.java
@@ -0,0 +1,10 @@
+package cn.netwins.allmedianews.base;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by desaco on 2018/11/5.
+ */
+
+public class BaseFragmentActivity extends FragmentActivity {
+}
