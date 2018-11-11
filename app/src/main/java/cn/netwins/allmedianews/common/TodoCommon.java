4ba897daec24bc2752c448ec6faa82ac2a2d6d17
desaco1989 <784967132@qq.com>
2018-11-11 16:52:26 +0800

    update

---
+++ app/src/main/java/cn/netwins/allmedianews/common/TodoCommon.java
@@ -0,0 +1,12 @@
+package cn.netwins.allmedianews.common;
+
+/**
+ * Created by desaco on 2018/11/11.
+ */
+
+public class TodoCommon {
+    // SharedPreferences, Json, Xml, File, Device, System, Log
+
+
+
+}
