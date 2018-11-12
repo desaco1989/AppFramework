3e8c10d7be68b6902c11b97c3d90aaedcf6e8938
unknown <dengwen@netwin.com>
2018-11-12 14:38:07 +0800

    update

---
+++ app/src/main/java/cn/netwins/allmedianews/adapter/MyAdapter.java
@@ -0,0 +1,8 @@
+package cn.netwins.allmedianews.adapter;
+
+/**
+ * Created by desaco on 2018/11/12.
+ */
+
+public class MyAdapter {
+}
---
+++ app/src/main/java/cn/netwins/allmedianews/widgets/CustomImageView.java
@@ -0,0 +1,8 @@
+package cn.netwins.allmedianews.widgets;
+
+/**
+ * Created by desaco on 2018/11/12.
+ */
+
+public class CustomImageView {
+}
